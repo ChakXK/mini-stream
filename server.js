9e9f94f91a3f8fdecf420f536d3133bf0912da12
Kseniia Kovalchak <kseniia_kovalchak@epam.com>
2022-02-17 20:14:58 +0200

    u

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/mini-stream'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/mini-stream/index.html'));});
-app.listen(process.env.PORT || 8080);
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/mini-stream'));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/mini-stream/'}
+  );
+});
+
+  app.listen(process.env.PORT || 8080);
